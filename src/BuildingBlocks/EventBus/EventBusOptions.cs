ff06e53cea27764620af24cae9537bf6067a6e0d
Fırat Şakar <sakarfiratt@gmail.com>
2026-06-06 14:32:35 +0300

    create EventBusOptions class for configuration

---
+++ src/BuildingBlocks/EventBus/EventBusOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace BuildingBlocks.EventBus;
+
+public sealed class EventBusOptions
+{
+    public const string SectionName = "EventBus";
+    public string Host { get; set; } = "localhost";
+    public int Port { get; set; } = 5672;
+    public string UserName { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+    public string VirtualHost { get; set; } = "/";
+    public int RetryCount { get; set; } = 3;
+    public string ExchangeName { get; set; } = "intelligent_incident_exchange";
+}
