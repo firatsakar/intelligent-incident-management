118c746794325184d2c004d60e3644fb99daa6da
Fırat Şakar <sakarfiratt@gmail.com>
2026-06-23 15:24:53 +0300

    Incident API layet infrastructure

---
+++ src/Services/IncidentService/IncidentService.API/Contracts/CreateIncidentRequest.cs
@@ -0,0 +1,12 @@
+﻿using IncidentService.Domain.Enums;
+
+namespace IncidentService.API.Contracts;
+
+public sealed record CreateIncidentRequest
+{
+    public required string Title { get; init; }
+    public required string Description { get; init; }
+    public required IncidentPriority Priority { get; init; }
+    public required IncidentSource Source { get; init; }
+    public string? AssignedTeam { get; init; }
+}
--- src/Services/IncidentService/IncidentService.API/Program.cs
+++ src/Services/IncidentService/IncidentService.API/Program.cs
@@ -1,23 +1,28 @@
+using BuildingBlocks.EventBus;
+using IncidentService.Application.Commands.CreateIncident;
+using IncidentService.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(
+        typeof(CreateIncidentCommand).Assembly));
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddRabbitMqEventBus(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
