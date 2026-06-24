b676cf8485625d703be2f15da1c0226008feda11
Fırat Şakar <sakarfiratt@gmail.com>
2026-06-24 15:28:16 +0300

    create request models for assign and update

---
+++ src/Services/IncidentService/IncidentService.API/Contracts/AssignTeamRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace IncidentService.API.Contracts;
+
+public sealed record AssignTeamRequest
+{
+    public required string Team { get; init; }
+}
---
+++ src/Services/IncidentService/IncidentService.API/Contracts/UpdateStatusRequest.cs
@@ -0,0 +1,8 @@
+﻿using IncidentService.Domain.Enums;
+
+namespace IncidentService.API.Contracts;
+
+public sealed record UpdateStatusRequest
+{
+    public required IncidentStatus NewStatus { get; init; }
+}
